02ae933092e55c05e86504cd0f6529df6b9b18b5
Gilson Junio Sales Andrade <gilsonjunio@quimicojapones.com.br>
2024-07-17 15:32:38 -0300

    ACHEI QUE HAVIA RESOLVIDO O PROBLEMA DA TELA DE LOGIN, MAS ELE PERMANECIA ACONTECENDO, COM ESSE COMMIT, O PROBLEMA ESTÁ PARCIALMENTE RESOLVIDO. QUANDO FAZER LOGIN, É NECESSÁRIO FAZER DUAS VEZES PARA FUNCIONAR. PROVAVELMENTE ISSO OCORRE DEVIDO AO FLUXO DO CÓDIGO, O QUAL DEPENDE DE LOCAL STORAGE E A API DO BANCO DE DADOS

--- login/script.js
+++ login/script.js
@@ -1,5 +1,5 @@
 // Nome, email, idade, cidade, foto e senha
-async function carregarDados(){
+/*async function carregarDados(){
     const apiUrl = "https://projeto-backend-five.vercel.app"
 
     try {
@@ -8,10 +8,12 @@ async function carregarDados(){
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ email: "gilsonjunio@exemplo.com" }),
+          body: JSON.stringify({ email: "gilsonjunio@exemplo.com", password:"Senha@123" }),
         });
+        console.log(response)
 
         const result = await response.json();
+        console.log(result)
 
         if (response.ok) {
           alert('Login bem sucedido!');
@@ -24,30 +26,65 @@ async function carregarDados(){
         console.error('Erro ao realizar login:', error);
       }
      //let response = await fetch('`${apiUrl}/login`')
-     console.log(response)
-     let data = await response.json()
-     let dados = data.alunos
+     //console.log(response)
+     //let data = await response.json()
+     //let dados = data.alunos
 
     // localStorage.setItem('usuarios',JSON.stringify(dados))
 }
-
+*/
 let container = document.getElementById('container')
-
-carregarDados()
 
 
 let botaoLogin = document.getElementById("botao_login");
 if(botaoLogin){ 
     botaoLogin.addEventListener('click', () => {
+            async function carregarDados(emailLogin,senhaLogin){
+        const apiUrl = "https://projeto-backend-five.vercel.app"
+
+        try {
+            const response = await fetch(`${apiUrl}/login`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ email: emailLogin, password:senhaLogin }),
+            });
+            console.log(response)
+
+            const result = await response.json();
+            console.log(result.user)
+
+            localStorage.setItem('usuarioLogado',JSON.stringify(result.user))
+            if (response.ok) {
+              alert('Login bem sucedido!');
+            } else {
+              alert(`Erro: ${result.error}`);
+            }
+
+            console.log(result);
+          } catch (error) {
+            console.error('Erro ao realizar login:', error);
+          }
+         //let response = await fetch('`${apiUrl}/login`')
+         //console.log(response)
+         //let data = await response.json()
+         //let dados = data.alunos
+
+        // localStorage.setItem('usuarios',JSON.stringify(dados))
+        }
         let emailLogin = document.getElementById("email_login").value
         let senhaLogin = document.getElementById("senha_login").value
 
-        let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-        let usuario = usuarios.find(usuario => usuario.email === emailLogin && usuario.senha === senhaLogin)
-            localStorage.setItem('usuarioLogado',JSON.stringify(usuario))
+        carregarDados(emailLogin,senhaLogin)
 
-        console.log(usuarios, emailLogin, senhaLogin, usuario)
-        if(usuario){
+        let usuario_logado = JSON.parse(localStorage.getItem("usuarioLogado")) || [];
+        let usuario_logado_email = usuario_logado.email
+        console.log(usuario_logado)
+        console.log(usuario_logado_email)
+
+        console.log( emailLogin, senhaLogin, usuario_logado, usuario_logado_email)
+        if(usuario_logado_email === emailLogin){
             window.location.href = "../boas_vindas/index.html"
         }
     })
